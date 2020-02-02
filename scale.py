011d7ff3b7f2d0328d7971e2deacfc2f29cb2566
Josie Barth <josie.barth@gmail.com>
2020-02-02 12:02:57 -0500

    Lets see if this now works, praygit status

--- scale.py
+++ scale.py
@@ -14,34 +14,20 @@ def helloWorld():
 
 @app.route('/scale/callback', methods=['POST']) 
 def writeScaleTaskToS3():
-
-    print("In /scale/callback")
-
+    
     ## Initialize S3 client
     s3_client = boto3.client('s3',
         aws_access_key_id=os.environ['S3_ACCESS_KEY'], 
         aws_secret_access_key=os.environ['S3_SECRET_KEY'], 
         region_name=os.environ['S3_REGION']
     )
-
-    print("Init S3")
-
-    print(request)
     
     ## Grab the callback JSON task sent from Scale
     task_json = request.get_json(force=True)
-    print(task_json)
-
     task_id = task_json.get('task_id')
-    print(task_id)
-
     task_json_serialized = json.dumps(task_json)
-    print(task_json_serialized)
 
     try:
-
-        print("Write to S3")
-
         ## Write the task to S3
         response = s3_client.put_object(
             Bucket=os.environ['S3_BUCKET_NAME'],
@@ -50,8 +36,6 @@ def writeScaleTaskToS3():
         );
         return jsonify([{'status':200, 'message':'Scale task written to S3'}])
     except ClientError as e: 
-        print("Fail write to S3")
-
         ## Log in case of error
         print(e)
         return jsonify([{'status':400, 'error':'Something went wrong writing scale task to S3'}])
